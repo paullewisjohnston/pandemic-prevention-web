3b37114207efd67f9f3f6d97e4ad6f04dbfe77a7
Paul Johnston <pjohnston571@hotmail.co.uk>
2020-03-23 19:20:47 +0000

    Added CNAME file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App" height='100vh' width='100vw'>
       <header className="App-header">
         <div style={{overflow:'hidden', height:'100%', width:'100%', zIndex:99, position: 'absolute'}}>
-          <video maxHeight='100%' maxWidth='100%' style={{ position: 'absolute', transform:'translate(-50%, -50%)', left:'50%', top:'50%'}} autoPlay loop>
+          <video maxHeight='100%' maxWidth='100%' style={{ position: 'absolute', transform:'translate(-50%, -50%)', left:'50%', top:'50%'}} autoPlay loop muted>
             <source src={hero} type="video/mp4"/>
           </video>
           <div style={{overflow:'hidden', backgroundColor:'black', opacity:'0.9', height:'100%', width:'100%', zIndex:1, position: 'absolute'}}/>
