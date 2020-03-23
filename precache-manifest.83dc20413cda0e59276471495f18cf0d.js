6c6254d67221e4b060450ebe2b959780cd7092dc
Paul Johnston <pjohnston571@hotmail.co.uk>
2020-03-23 16:07:34 +0000

    Updates

---
+++ precache-manifest.83dc20413cda0e59276471495f18cf0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7adca5bb91313ebc4f86eaca4e32d576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6982f06676ca0d540094",
+    "url": "/static/css/main.b116ca6a.chunk.css"
+  },
+  {
+    "revision": "8229203e627e334bb982",
+    "url": "/static/js/2.d383ecdc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d383ecdc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6982f06676ca0d540094",
+    "url": "/static/js/main.03ce648b.chunk.js"
+  },
+  {
+    "revision": "ebd14d9cfad1ec42057e",
+    "url": "/static/js/runtime-main.5aa13656.js"
+  },
+  {
+    "revision": "288f50fc6355aa14abc20fb539b05be7",
+    "url": "/static/media/logo.288f50fc.svg"
+  },
+  {
+    "revision": "6fb0689b010367b6d54ce894b0b35d64",
+    "url": "/static/media/pp_hero.6fb0689b.mov"
+  }
+]);
