b352f8ce90ff81c407193a1d19c2d81780d36ddf
Paul Johnston <pjohnston571@hotmail.co.uk>
2020-03-23 15:47:56 +0000

    Push build folder

---
+++ build/precache-manifest.18267e617cafa0a4562079b50b1719b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a96fc28e24fb527c8b845a5b9b9972f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4faafe87b86d2273b7d6",
+    "url": "/static/css/main.a2befcc5.chunk.css"
+  },
+  {
+    "revision": "a6334988f81d6f0749e8",
+    "url": "/static/js/2.2607973a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2607973a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4faafe87b86d2273b7d6",
+    "url": "/static/js/main.7b00a99d.chunk.js"
+  },
+  {
+    "revision": "83daa556700b9bc63951",
+    "url": "/static/js/runtime-main.16698ab1.js"
+  },
+  {
+    "revision": "288f50fc6355aa14abc20fb539b05be7",
+    "url": "/static/media/logo.288f50fc.svg"
+  },
+  {
+    "revision": "6fb0689b010367b6d54ce894b0b35d64",
+    "url": "/static/media/pp_hero.6fb0689b.mov"
+  }
+]);
