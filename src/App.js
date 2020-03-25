26598c26743d19418a3cf352af662b5f8f6bb178
Paul Johnston <pjohnston571@hotmail.co.uk>
2020-03-25 11:03:48 +0000

    Updated styling for mobile

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import hero from './pp_hero.mov';
 
 function App() {
   return (
-    <div className="App" height='100vh' width='100vw' overflow='hidden'>
-        <div style={{overflow:'hidden', height:'100%', width:'100%', zIndex:99, position: 'absolute'}}>
-          <video maxheight='100%' maxwidth='100%' style={{ position: 'absolute', transform:'translate(-50%, -50%)', left:'50%', top:'50%'}} autoPlay loop muted>
+    <div className="App">
+      <div className="outer-container">
+        <div className="video-container">
+          <div className="video-overlay"/>
+          <div className="video-overlay-content">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Pandemic Prevention</h1>
+            <p>Coming Soon: IOS & Android</p>
+          </div>
+          <video className="video" src={hero} autoPlay loop muted/>
+        </div>
+      </div>
+
+
+        {/* <div style={{ height:'100%', width:'100%', zIndex:99, position: 'absolute'}}>
+          <video style={{ position: 'absolute', transform:'translate(-50%, -50%)', left:'50%', top:'50%'}} autoPlay loop muted>
             <source src={hero} type="video/mp4"/>
           </video>
-          <div style={{overflow:'hidden', backgroundColor:'black', opacity:'0.9', height:'100%', width:'100%', zIndex:1, position: 'absolute'}}/>
-          <div style={{overflow:'hidden', height:'100%', width:'90%', zIndex:98, position: 'absolute', transform:'translate(-50%, -50%)', left:'50%', top:'70%'}}>
+          <div style={{backgroundColor:'black', opacity:'0.9', height:'100%', width:'100%', zIndex:1, position: 'absolute'}}/>
+          <div style={{ height:'100%', width:'90%', zIndex:98, position: 'absolute', transform:'translate(-50%, -50%)', left:'50%', top:'70%'}}>
               <img src={logo} className="App-logo" alt="logo" />
                 <h1>Pandemic Prevention</h1>
                 <p>Coming Soon: IOS & Android</p>
           </div>
-        </div>
+        </div> */}
     </div>
   );
 }
